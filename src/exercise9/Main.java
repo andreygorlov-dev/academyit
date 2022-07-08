e7de21bcd78ce36a3dcf1f740d6c43e906e2ca80
Andrey Gorlov <andreygorlov19995@gmail.com>
2022-07-09 00:55:31 +0300

    ДЗ №9

---
+++ src/exercise9/Main.java
@@ -0,0 +1,27 @@
+package exercise9;
+
+import java.util.stream.IntStream;
+
+/**
+ * Изменить алгоритм, используя Stream API
+ * int[] arr = {50, 60, 70, 80, 90, 100, 110, 120};
+ * int count = 0;
+ * for (int x : arr) {
+ *  if (x >= 80) continue;
+ *  x += 10;
+ *  count++;
+ *  if (count > 2) break;
+ *  System.out.print(x);
+ * }
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        IntStream.of(50, 60, 70, 80, 90, 100, 110, 120)
+                .filter(x -> x < 80)
+                .map(x -> x + 10)
+                .limit(2)
+                .forEach(System.out::print);
+    }
+
+}
