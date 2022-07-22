988f9137c693e4e340d5bef32f0527b8b1ab4b0e
Andrey Gorlov <andreygorlov19995@gmail.com>
2022-07-23 00:26:17 +0300

    ДЗ 11

---
+++ src/exercise11/CalcWindow.java
@@ -0,0 +1,232 @@
+package exercise11;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Set;
+
+/**
+ * 1) Привести внешний вид калькулятора к виду Windows калькулятора,
+ * добавив кнопки для корня квадратного и др.
+ * 2) Сделать рефакторинг кода в примере калькулятора. Применить методы.
+ */
+public class CalcWindow extends JFrame {
+
+    private JPanel contentPane;
+    private JTextField tfResult; // Строка отображающая результат
+    private JLabel lbOperation; // Для отображения последней операции
+    private JMenuBar menuBar;
+    private JMenu mMenu;
+    private JMenuItem item1;
+    private JMenuItem item2;
+    private int operation; // Код операции
+    private JButton[] btn;
+    private Listener listener;
+    private String textResult; // результат, который выводится в текстовое поле
+    private double number1; // Первое введенное число
+    private double number2; // Второе введенное число
+    private String[] btnLabels = {"C", "<-", "xʸ", "√",
+            "7", "8", "9", "/",
+            "4", "5", "6", "*",
+            "1", "2", "3", "+",
+            "0", ".", "=", "-"};
+
+    public CalcWindow() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBounds(100, 100, 315, 428);
+
+        listener = new Listener();
+
+        menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        Font menuFont = new Font("serif", Font.BOLD, 15);
+        mMenu = new JMenu("Меню");
+        menuBar.add(mMenu);
+        item1 = new JMenuItem("Справка");
+        item1.setFont(menuFont);
+        item1.addActionListener(listener);
+        mMenu.add(item1);
+        item2 = new JMenuItem("Выход");
+        item2.setFont(menuFont);
+        item2.addActionListener(listener);
+        mMenu.add(item2);
+
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        tfResult = new JTextField();
+        tfResult.setBounds(12, 28, 276, 52);
+        tfResult.setFont(new Font("serif", Font.BOLD, 30));
+        contentPane.add(tfResult);
+
+        lbOperation = new JLabel("");
+        lbOperation.setBounds(156, 12, 132, 15);
+        contentPane.add(lbOperation);
+
+        btn = new JButton[20];
+        Font btnFont = new Font("serif", Font.BOLD, 15); // Шрифт для кнопок
+        int x = 12, y = 110;//Первоначальные координаты кнопок
+        for (int i = 0; i < btn.length; i++) { // Циклом расставляем кнопки
+            btn[i] = new JButton();
+            btn[i].setLocation(x, y);
+            btn[i].setSize(60, 40);
+            btn[i].setFont(btnFont);
+            if (i >= 4 && i <= 6 || i >= 8 && i <= 10 || i >= 12 && i <= 14 || i == 16) { // Оформление для цифровых кнопок
+                btn[i].setBackground(Color.darkGray);
+                btn[i].setForeground(Color.white);
+            } else if (i == 18) { // Оформление для кнопки "="
+                btn[i].setBackground(Color.blue);
+                btn[i].setForeground(Color.white);
+            } else { // Оформление для остальных кнопок
+                btn[i].setBackground(Color.gray);
+                btn[i].setForeground(Color.white);
+            }
+            btn[i].setText(btnLabels[i]);
+
+            x += 72;
+            if ((i + 1) % 4 == 0) { // Когда i + 1 кратно 4 ( если i = 3, то это 4 кнопка в ряду) пенеосим распределение кнопок на следующий ряд
+                x = 12;
+                y += 52;
+            }
+            btn[i].addActionListener(listener); // Создаем слушателя событий
+            contentPane.add(btn[i]); // добавляем кнопки
+        }
+    }
+
+    class Listener implements ActionListener {
+        private int position; // позиция знака операции, чтобы парсить второе введенное число
+        private JButton btn;
+        private String text;
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == item1) {
+                String info = "Гид по операциям:\n\n\"xʸ\" - возведение в степень(ввести число, нажать \"xʸ\", ввести число степени)\n"
+                        + "\"√\" - квадратный корень из введенного числа(ввести число, нажать \"√\")\n\"/\" - деление(ввести число, нажать \"/\", ввести второе число, нажать \"=\")"
+                        + "\n\"*\" - умножение(ввести число, нажать \"*\", ввести второе число, нажать \"=\")\n\"+\" - сложение(ввести число, нажать \"+\", ввести второе число, нажать \"=\")"
+                        + "\n\"-\" - вычитание(ввести число, нажать \"-\", ввести второе число, нажать \"=\")\n\n\"C\" - очищает поле\n\"<-\" - удаляет последний символ в поле\n";
+                JOptionPane.showMessageDialog(null, info);
+            } else if (e.getSource() == item2) {
+                System.exit(0);
+            } else {
+                btn = (JButton) e.getSource();
+                text = btn.getText();
+                if (text.equalsIgnoreCase("C")) { // Очищаем текстовое поле
+                    tfResult.setText("");
+                    number1 = 0;
+                    number2 = 0;
+
+                } else if (text.equals("<-")) { // После нажатия кнопки удаляем из строки последний символ
+                    isNull(tfResult.getText());
+                    tfResult.setText(tfResult.getText().substring(0, tfResult.getText().length() - 1));
+
+                } else if (text.equals("+")) {
+                    operation = 1;
+                    concatTextField(); // Строим в текстовом поле строку с вычислениями (для наглядности операции)
+
+                } else if (text.equals("-")) {
+                    operation = 2;
+                    concatTextField();
+
+                } else if (text.equals("*")) {
+                    operation = 3;
+                    concatTextField();
+
+                } else if (text.equals("/")) {
+                    operation = 4;
+                    concatTextField();
+
+                } else if (text.equals("xʸ")) {
+                    operation = 5;
+                    concatTextField();
+
+                } else if (text.equals("√")) {
+                    operation = 6;
+                    number1 = Double.parseDouble(tfResult.getText());
+                    textResult = runOperation();
+                    lbOperation.setText("√" + tfResult.getText() + "=" + textResult); // Сохраняем последнюю операцию
+                    tfResult.setText(textResult);
+
+                } else if (text.equals("=")) {
+                    String operands = tfResult.getText(); // сохраняем текст до нажатия на кнопку "="
+                    number2 = Double.parseDouble(tfResult.getText().substring(position, tfResult.getText().length()));
+                    textResult = runOperation();
+                    lbOperation.setText(operands + "=" + tfResult.getText()); // Сохраняем последнюю операцию
+                    tfResult.setText(textResult);
+
+                } else {
+                    tfResult.setText(tfResult.getText() + text);
+                }
+            }
+        }
+
+        private void isNull(String text) { // Метод проверяет, есть ли в текстовом поле что то набранное перед
+            // совершением операции
+            if (text.isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Нет введенного числа"); // Если нет то выводит предупреждение
+            }
+        }
+
+        private boolean isFractNum(double num) { // Метод проверяет дробную часть числа
+            double fractionPart = num - (int) num;
+            return fractionPart > 0; // возвращает true, если дробная часть > 0, иначе false
+        }
+
+        private void concatTextField() { // собираем текст в текстовом поле
+            isNull(tfResult.getText());
+            number1 = Double.parseDouble(tfResult.getText()); // Парсим первое введенное число
+            tfResult.setText(tfResult.getText() + (text.equals("xʸ") ? "^" : text)); // Устанавливаем текст: первое число и знак операции (symb)
+            position = tfResult.getText().length(); // устанавливаем позицию знака операции. В дальнейшем, при парсинге
+            // второго числа, отталкиваемся от этой позиции
+        }
+
+        private String runOperation() { // Метод совершает вычисление в зависимости от выбранной операции. Возвращает отформатированную строку, которую передаем в текстовое поле как результат
+            double result = 0;
+            switch (operation) {
+                case 1:
+                    result = number1 + number2;
+                    break;
+                case 2:
+                    result = number1 - number2;
+                    break;
+                case 3:
+                    result = number1 * number2;
+                    break;
+                case 4:
+                    if (number2 == 0) {
+                        try {
+                            throw new Exception("На 0 делить нельзя!");
+                        } catch (Exception e1) {
+                            JOptionPane.showMessageDialog(null, e1.getMessage());
+                        }
+                    }
+                    result = number1 / number2;
+                    break;
+                case 5:
+                    result = Math.pow(number1, number2);
+                    break;
+                case 6:
+                    result = Math.sqrt(number1);
+                    break;
+                default:
+                    break;
+            }
+            result = Math.rint(10000000 * result) / 10000000; // Таким образом устанавливается максимальное количество цифр после запятой
+            return (isFractNum(result)) ? "" + result : "" + (int) result;// Если число дробное (12.3), то возвращаем его таким, если число целое (12.0), то отсекаем дробную часть.
+        }
+    }
+
+    public static void main(String[] args) {
+        CalcWindow frame = new CalcWindow();
+        frame.setTitle("Калькулятор");
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+
+}
